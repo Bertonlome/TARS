18f7756e6b48e41016c4081646e4beeba3a42314
Benjamin R. Berton <benjaminberton64@gmail.com>
2026-05-14 18:33:27 -0400

    p18

--- Core/agent.py
+++ Core/agent.py
@@ -36,7 +36,7 @@ elif platform.system() == "Windows":
 else:
     DEFAULT_DEVICE = "wlps"
 
-CURRENT_BRIEFING_EXPORT_LOADED = "briefing_export_20260424_121306.csv"
+CURRENT_BRIEFING_EXPORT_LOADED = "briefing_export_20260514_174207.csv"
 #CURRENT_BRIEFING_EXPORT_LOADED = "briefing_export_FULL_TARS_PERF.csv"
 ### PARAMETERS ###
 ALLOW_PARALLEL_ATC = True  # Enable/disable parallel ATC thread execution
